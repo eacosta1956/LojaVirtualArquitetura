7a51a2cfa64b3df9f19f50951127e2447c6142c3
eacosta1956 <eldon.costa@terra.com.br>
2026-03-18 01:36:32 -0300

    feat: implementar UsuariosActivity e finalizar listagem com Glide

--- app/src/main/java/com/eldoncosta/lojavirtualarquitetura/ui/usuarios/UsuariosActivity.kt
+++ app/src/main/java/com/eldoncosta/lojavirtualarquitetura/ui/usuarios/UsuariosActivity.kt
@@ -1,12 +1,14 @@
 package com.eldoncosta.lojavirtualarquitetura.ui.usuarios
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.eldoncosta.lojavirtualarquitetura.databinding.ActivityUsuariosBinding
 
 class UsuariosActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityUsuariosBinding
+    private val viewModel: UsuarioViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +18,14 @@ class UsuariosActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // Configura um título na barra superior para teste
-        supportActionBar?.title = "Usuários"
+        supportActionBar?.title = "Lista de Usuários"
+
+        // Observa a lista de usuários e atualiza o Adapter
+        viewModel.usuarios.observe(this) { lista ->
+            binding.rvUsuarios.adapter = UsuarioAdapter(lista)
+        }
+
+        // Busca os usuários na API
+        viewModel.carregarUsuarios()
     }
 }
