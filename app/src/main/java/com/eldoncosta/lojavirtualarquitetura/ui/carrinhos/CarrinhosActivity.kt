9e118e0eb2d4eede117b1cfddc76772b870b51b7
eacosta1956 <eldon.costa@terra.com.br>
2026-03-18 00:52:16 -0300

    feat: implementar RecyclerView na CarrinhosActivity e finalizar listagem

--- app/src/main/java/com/eldoncosta/lojavirtualarquitetura/ui/carrinhos/CarrinhosActivity.kt
+++ app/src/main/java/com/eldoncosta/lojavirtualarquitetura/ui/carrinhos/CarrinhosActivity.kt
@@ -1,12 +1,14 @@
 package com.eldoncosta.lojavirtualarquitetura.ui.carrinhos
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.eldoncosta.lojavirtualarquitetura.databinding.ActivityCarrinhosBinding
 
 class CarrinhosActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCarrinhosBinding
+    private val viewModel: CarrinhoViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +19,13 @@ class CarrinhosActivity : AppCompatActivity() {
 
         // Configura um título na barra superior para teste
         supportActionBar?.title = "Carrinhos"
+
+        // Observa a lista de carrinhos vinda do ViewModel
+        viewModel.carrinhos.observe(this) { lista ->
+            binding.rvCarrinhos.adapter = CarrinhoAdapter(lista)
+        }
+
+        // Inicia a busca dos dados na API
+        viewModel.carregarCarrinhos()
     }
 }
